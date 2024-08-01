779b82b4135845dad0cbc59368c8d63d25cffbf5
ZaninDe <gabriel.zaninde@gmail.com>
2024-07-31 21:49:46 -0300

    remove char

--- app/(site)/_components/AuthForm.tsx
+++ app/(site)/_components/AuthForm.tsx
@@ -119,7 +119,7 @@ const AuthForm = () => {
   }
   return (
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10 max-w-[90%]">
+      <div className="bg-white mx-4 md:mx-0 md:px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             {variant === 'REGISTER' && (
